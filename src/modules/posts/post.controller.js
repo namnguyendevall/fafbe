fca0097d53427f2a2ab3934468264ea2430231a8
nguyenquocnamfe <binbibna@gmail.com>
2026-03-15 00:30:45 +0700

    Add verbose error logging for posts feed

--- src/modules/posts/post.controller.js
+++ src/modules/posts/post.controller.js
@@ -50,7 +50,8 @@ const getFeed = async (req, res, next) => {
         const posts = await postService.getFeed(req.user.id, page, limit);
         res.status(200).json({ data: posts });
     } catch (error) {
-        next(error);
+        console.error('[getFeed] Error:', error.message);
+        res.status(500).json({ message: 'Internal server error', error: error.message });
     }
 };
 
