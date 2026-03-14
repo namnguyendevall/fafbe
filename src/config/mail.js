a59c518dfb99043067c891f104aa6b3e89707c7f
nguyenquocnamfe <binbibna@gmail.com>
2026-03-15 03:07:58 +0700

    Debug: Add logs to verify mailer environment variables

--- src/config/mail.js
+++ src/config/mail.js
@@ -6,9 +6,11 @@ const transporter = nodemailer.createTransport({
     user: process.env.MAIL_USER,
     pass: process.env.MAIL_PASS,
   },
-  connectionTimeout: 5000,
-  greetingTimeout: 5000,
-  socketTimeout: 10000,
+  connectionTimeout: 10000,
+  greetingTimeout: 10000,
+  socketTimeout: 20000,
 });
+
+console.log('[Mailer Config] Host: gmail, User:', process.env.MAIL_USER ? 'SET' : 'MISSING');
 
 module.exports = transporter;
