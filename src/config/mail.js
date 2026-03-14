3fbe3f1145945559df6e36f613eade68a9813bba
nguyenquocnamfe <binbibna@gmail.com>
2026-03-15 01:27:03 +0700

    Fix: Add SMTP timeouts and explicit config to resolve OTP hang

--- src/config/mail.js
+++ src/config/mail.js
@@ -1,11 +1,16 @@
 const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true, // Use SSL
   auth: {
     user: process.env.MAIL_USER,
     pass: process.env.MAIL_PASS,
   },
+  connectionTimeout: 10000, // 10 seconds
+  greetingTimeout: 5000,
+  socketTimeout: 15000,
 });
 
 module.exports = transporter;
